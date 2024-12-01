d95102170930fc455d56d4f89e7bd0976e7b76ec
dsycrs <daisymen16@gmail.com>
2024-12-01 13:57:35 -0600

    primer commit

---
+++ src/logica/LaVieGourmetReservas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+import giu.Menu;
+
+/**
+ *
+ * @author Daisy Ccaceres
+ */
+public class LaVieGourmetReservas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+    
+}
